2b58ae15165b5d103cc50a7358fe3cf6d12edc52
jeanunicesumar <124851594+jeanunicesumar@users.noreply.github.com>
2024-05-24 00:59:17 -0300

    Finalizando

--- pages/to-do/index.js
+++ pages/to-do/index.js
@@ -1,47 +1,101 @@
-function enviarForm(e) {
-  e.preventDefault();
+const taskKey = '@tasks'
 
-  const form = e.target;
-  const formData = new FormData(form);
+function addTask(event) {
+  event.preventDefault() // Evita o recarregamento da página
+  const taskId = new Date().getTime()
+  const taskList = document.querySelector('#taskList')
 
-  const task = formData.get('title');
-  const description = formData.get('description');
+  const form = document.querySelector('#taskForm')
+  const formData = new FormData(form)
 
-  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const taskTitle = formData.get('title')
+  const taskDescription = formData.get('description')
 
-  const objectTask = { title: task, description: description };
-  tasks.push(objectTask);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+  tasks.push({ id: taskId, title: taskTitle, description: taskDescription })
+  localStorage.setItem(taskKey, JSON.stringify(tasks))
 
-  form.reset();
-  showListTask();
+  appendTasksLi();
+  form.reset()
 }
 
-function showListTask() {
-
-  
-  const divList = document.getElementById("list-task");
-  divList.textContent = "";
-  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-  tasks.forEach(task => {
-    
-    const div = document.createElement('div');
-    const title = document.createElement('h2');
-    title.textContent = task.title
-
-    const p = document.createElement('p');
-    p.textContent = task.description
-
-    div.appendChild(title);
-    div.appendChild(p);
-
-    divList.appendChild(div);
-
-  });
-
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  showListTask();
+window.addEventListener('DOMContentLoaded', () => {
+  appendTasksLi();
 })
+
+function appendTasksLi() {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+  const taskList = document.querySelector('#taskList')
+  taskList.innerHTML = tasks
+    .map((task) => `<li id="${task.id}">
+                      <h2>${task.title}</h2><p>${task.description}</p>
+                      <button class="button-edit" title="Editar tarefa">✏️</button>
+                    </li>`)
+    .join('');
+
+  insertEventsInButtons();
+}
+
+function insertEventsInButtons() {
+  document.querySelectorAll('.button-edit').forEach(button => {
+    button.addEventListener('click', () => openModalEdit(button.parentElement.id));
+});
+}
+
+function openModalEdit(id) {
+  const dialog = document.querySelector('.dialog');
+
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+  let task = tasks.filter(task => task.id == id);
+
+  if (task.length === 0) {
+    return;
+  }
+  
+  task = task[0];
+
+  const titleEdit = document.querySelector('#title-edit');
+  titleEdit.value = task.title;
+  const descriptionEdit = document.querySelector('#description-edit');
+  descriptionEdit.value = task.description;
+
+  dialog.id = id;
+  dialog.showModal();
+
+}
+
+function resetDialog(event) {
+  event.preventDefault();
+
+  const dialog = document.querySelector('.dialog');
+  dialog.close();
+}
+
+function editTask(event) {
+  event.preventDefault();
+
+  const dialog = document.querySelector('.dialog');
+
+  const form = document.querySelector('#taskFormEdit')
+  const formData = new FormData(form)
+
+  const taskTitle = formData.get('title')
+  const taskDescription = formData.get('description')
+
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+  let task = tasks.filter(task => task.id == dialog.id);
+
+  if (task.length === 0) {
+    return;
+  }
+  
+  task = task[0];
+  task.title = taskTitle;
+  task.description = taskDescription;
+
+  localStorage.setItem(taskKey, JSON.stringify(tasks));
+  appendTasksLi();
+
+  dialog.removeAttribute('id');
+  dialog.close();
+}
